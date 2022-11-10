0fc85de34dc065972c5eb97b57ef2a04484d8911
Frank <hello@zwb.com>
2022-11-10 11:28:16 +0800

    Initial commit

---
+++ src/FileDemo.java
@@ -0,0 +1,40 @@
+import java.io.File;
+
+public class FileDemo {
+    public static void main(String[] args) {
+//        //File(String pathname)：通过将给定的路径名字符串转换为抽象路径名来创建新的 File实例。
+//        File f1 = new File("E:\\itcast\\java.txt");
+//        System.out.println(f1);
+
+//          File(String parent, String child)：从父路径名字符串和子路径名字符串创建新的File实例。
+//        File f2 = new File("E:\\itcast","java.txt");
+//        System.out.println(f2);
+
+//          File(File parent, String child)：从父抽象路径名和子路径名字符串创建新的 File实例。
+//        File f3 = new File("E:\\itcast");
+//        File f4 = new File(f3,"java.txt");
+//        System.out.println(f4);
+
+
+//      public boolean createNewFile()当具有该名称的文件不存在时，创建一个由该抽象路径名命名的新空文件
+//        public boolean mkdir() 创建由此抽象路径名命名的目录
+//        public boolean mkdirs() 创建由此抽象路径名命名的目录，包括任何必需但不存在的父目录
+
+//        public boolean isDirectory() 测试此抽象路径名表示的File是否为目录
+//        public boolean isFile() 测试此抽象路径名表示的File是否为文件
+//        public boolean exists() 测试此抽象路径名表示的File是否存在
+
+//        public String getAbsolutePath() 返回此抽象路径名的绝对路径名字符串
+//        public String getPath() 将此抽象路径名转换为路径名字符串
+//        public String getName() 返回由此抽象路径名表示的文件或目录的名称
+//        public String[] list() 返回此抽象路径名表示的目录中的文件和目录的名称字符串数组
+//        public File[] listFiles() 返回此抽象路径名表示的目录中的文件和目录的File对象数组
+
+//        public boolean delete() 删除由此抽象路径名表示的文件或目录
+//        File f1= new File("C:\\Users\\Administrator\\Desktop\\211学期学习\\Maven&MyBatis\\day04-1-Maven\\资料\\3. 测试web项目\\file\\java.txt");
+//        System.out.println(f1.delete());
+//
+//        File f2 = new File("C:\\Users\\Administrator\\Desktop\\211学期学习\\Maven&MyBatis\\day04-1-Maven\\资料\\3. 测试web项目\\file");
+//        System.out.println(f2.delete());
+    }
+}
